f363758bc0d401915b29e5525d24442f9fdb01d0
Chris Behrens <chrisbehrens@yahoo.com>
2023-04-26 09:04:04 -0700

    added reverseDoublyLinked list algo

--- src/doublyLinkedList/main.java
+++ src/doublyLinkedList/main.java
@@ -4,14 +4,14 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		doublyLinkedList doubleList = new doublyLinkedList(5);
-		doubleList.getHead();
-		doubleList.getTail();
-		doubleList.getLength();
-		doubleList.printList();
-		doubleList.append(69);
-		doubleList.append(75);
-		doubleList.prepend(88);
+//		doublyLinkedList doubleList = new doublyLinkedList(5);
+//		doubleList.getHead();
+//		doubleList.getTail();
+//		doubleList.getLength();
+//		doubleList.printList();
+//		doubleList.append(69);
+//		doubleList.append(75);
+//		doubleList.prepend(88);
 		
 //		doubleList.getHead();
 //		doubleList.getTail();
@@ -27,15 +27,22 @@ public class main {
 //		System.out.println(doubleList.removeFirst().value);
 //		System.out.println(doubleList.removeFirst().value);
 //		System.out.println(doubleList.removeFirst().value);
-		System.out.println(doubleList.get(1).value);
-		System.out.println(doubleList.set(2,999));
-		doubleList.printList();
-		System.out.println();
-		doubleList.insert(0,1000);
-		doubleList.printList();
-		doubleList.remove(2);
-		System.out.println();
-		doubleList.printList();
+//		System.out.println(doubleList.get(1).value);
+//		System.out.println(doubleList.set(2,999));
+//		doubleList.printList();
+//		System.out.println();
+//		doubleList.insert(0,1000);
+//		doubleList.printList();
+//		doubleList.remove(2);
+//		System.out.println();
+//		doubleList.printList();
+		
+		reverserDoublyLinkedList newDouble = new reverserDoublyLinkedList(5);
+//		newDouble.append(77);
+//		newDouble.append(99);
+//		newDouble.append(888);
+//		newDouble.printList();
+		newDouble.reverse();
+		newDouble.printList();
 	}
-
 }
