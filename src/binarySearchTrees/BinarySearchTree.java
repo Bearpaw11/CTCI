830d29b7da8aedcaaeffd71853651c1e130ab901
Chris Behrens <chrisbehrens@yahoo.com>
2023-05-01 15:20:33 -0700

    added BinarySearchTree

---
+++ src/binarySearchTrees/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package binarySearchTrees;
+
+public class BinarySearchTree {
+	Node root;
+	
+	
+	class Node {
+		int value;
+		Node left;
+		Node right;
+		
+		private Node(int value) {
+			this.value = value;
+		}
+	}
+	
+}
---
+++ src/binarySearchTrees/Main.java
@@ -0,0 +1,13 @@
+package binarySearchTrees;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BinarySearchTree myBST = new BinarySearchTree();
+		System.out.println("Root = " + myBST.root);
+
+	}
+
+}
