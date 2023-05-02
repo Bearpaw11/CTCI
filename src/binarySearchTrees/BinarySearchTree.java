12bdc3fcfc534593a8b9a1dd7eeb0e7b474ec0bd
Chris Behrens <chrisbehrens@yahoo.com>
2023-05-02 12:53:11 -0700

    added binarySearchTree class

--- src/binarySearchTrees/BinarySearchTree.java
+++ src/binarySearchTrees/BinarySearchTree.java
@@ -14,4 +14,50 @@ public class BinarySearchTree {
 		}
 	}
 	
-}
+	//create new node
+	//check if tree is empty
+	//temp = root
+	//while loop
+	// if temp == new node return false
+	//compare new node to root node
+		//if spot is  null insert new node
+	public boolean insert(int value) {
+		Node newNode = new Node(value);
+		if(root == null) {
+			root = newNode;
+			return true;
+		}
+		Node temp = root;
+		while(true) {
+			if(newNode.value == temp.value) return false;
+			if(newNode.value < temp.value) {
+				if(temp.left == null) {
+					temp.left = newNode;
+					return true;
+				}
+				temp = temp.left;
+			} else {
+				if(temp.right == null) {
+				   temp.right = newNode;
+				   return true;
+			    }
+				temp = temp.right;
+			}
+		}
+	}
+	
+	public boolean contains(int value) {
+		Node temp = root;
+		while(temp != null) {
+			if(value < temp.value ) {
+				temp = temp.left;
+			} else if(value > temp.value){
+				temp = temp.right;
+			} else {
+				return true;
+			}
+			
+		}
+		return false;
+	}
+}	
